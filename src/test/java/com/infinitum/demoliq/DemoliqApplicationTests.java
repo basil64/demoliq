34dd709adcf562510986d4f712d3e8809cd416e6
przi <piotr.ziembicki@gft.com>
2018-07-19 16:40:26 +0200

    Properties files based on profiles added

--- src/test/java/com/infinitum/demoliq/DemoliqApplicationTests.java
+++ src/test/java/com/infinitum/demoliq/DemoliqApplicationTests.java
@@ -3,10 +3,14 @@ package com.infinitum.demoliq;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
+@Profile("test")
 public class DemoliqApplicationTests {
 
 	@Test
